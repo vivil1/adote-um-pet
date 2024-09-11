f3ececbc4a689de0e675235fee0913024d8d907e
vivil1 <106763955+vivil1@users.noreply.github.com>
2024-09-11 14:12:47 -0300

    hgh

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
-// Array de dados dos animais
 const animals = [
     {
         name: "Cachorro 1",
-        description: "Um cachorro calmo e amoroso, ele está velhinho, mas já está na hora de conhecer o que é o amor. Porte M.",
-        imageUrl: "images/IMG-20240911-WA0042.jpg"
-        whatsapp: "https://wa.me/5541999446689?text=Estou%20interessado%20em%20adotar%20o%20Caramelo"
-    },
-    {
-        name: "Cachorro 2",
         description: "Um cachorro calmo e amoroso, ele está velhinho, mas já está na hora de conhecer o que é o amor. Porte M.",
         imageUrl: "images/IMG-20240911-WA0043.jpg",
         whatsapp: "https://wa.me/5541999446689?text=Estou%20interessado%20em%20adotar%20o%20Caramelo"
     },
     {
-        name: "Cachorro 3",
+        name: "Cachorro 2",
         description: "Um cachorro calmo e amoroso, ele está velhinho, mas já está na hora de conhecer o que é o amor. Porte M.",
-        imageUrl: "images/IMG-20240911-WA0044",
+        imageUrl: "images/IMG-20240911-WA0042.jpg",
         whatsapp: "https://wa.me/5541999446689?text=Estou%20interessado%20em%20adotar%20o%20Caramelo"
+    },
+    {
+        name: "Cachorro 3",
+        description: "Cãozinho amigável e amoroso, porte M, resgatado das enchentes do RS.",
+        imageUrl: "images/IMG-20240911-WA0044.jpg",
+        whatsapp: "https://wa.me/5541999446689?text=Estou%20interessado%20em%20adotar%20o%20Sandro"
     },
     {
         name: "Cachorro 4",
         description: "Está à procura de um lar cheio de amor e carinho, foi resgatado pelo instituto SOS 4 patas.",
-        imageUrl: "images/IMG-20240911-WA0045",
+        imageUrl: "images/IMG-20240911-WA0045.jpg",
         whatsapp: "https://wa.me/5541999446689?text=Estou%20interessado%20em%20adotar%20o%20Suzão"
     },
     {
-        name: "cachorro 5",
+        name: "Cachorro 5",
         description: "Cãozinho amigável e amoroso, porte M, resgatado das enchentes do RS.",
-        imageUrl: "images/IMG-20240911-WA0046",
+        imageUrl: "images/IMG-20240911-WA0046.jpg",
         whatsapp: "https://wa.me/5541999446689?text=Estou%20interessado%20em%20adotar%20o%20Sandro"
     },
     {
-        name: "cachorro 6",
+        name: "Cachorro 6",
         description: "Cãozinho amigável e amoroso, porte M, resgatado das enchentes do RS.",
-        imageUrl: "images/IMG-20240911-WA0047",
+        imageUrl: "images/IMG-20240911-WA0047.jpg",
         whatsapp: "https://wa.me/5541999446689?text=Estou%20interessado%20em%20adotar%20o%20Sandro"
     },
     {
-        name: "cachorro 7",
+        name: "Cachorro 7",
         description: "Cãozinho amigável e amoroso, porte M, resgatado das enchentes do RS.",
         imageUrl: "images/IMG_20240911_101358_541.jpg",
         whatsapp: "https://wa.me/5541999446689?text=Estou%20interessado%20em%20adotar%20o%20Sandro"
     },
     {
-        name: "cachorro 8",
+        name: "Cachorro 8",
         description: "Cãozinho amigável e amoroso, porte M, resgatado das enchentes do RS.",
         imageUrl: "images/IMG_20240911_101405_424.jpg",
         whatsapp: "https://wa.me/5541999446689?text=Estou%20interessado%20em%20adotar%20o%20Sandro"
     }
 ];
+
+    // Adicione mais animais conforme necessário
 
 let currentAnimalIndex = 0;
 
@@ -72,20 +73,4 @@ function showAnimal() {
 document.getElementById('swipe-left').addEventListener('click', function() {
     currentAnimalIndex = (currentAnimalIndex + 1) % animals.length;
     showAnimal();
-});
-
-// Função para redirecionar ao WhatsApp (❤️)
-document.getElementById('swipe-right').addEventListener('click', function() {
-    const animal = animals[currentAnimalIndex];
-    window.location.href = animal.whatsapp;
-});
-
-// Função para mostrar informações adicionais
-document.getElementById('show-denuncia').addEventListener('click', function() {
-    showInfo("Denúncia de Maus Tratos", "Aqui você encontrará informações sobre como denunciar maus-tratos a animais. É importante saber como proceder caso presencie ou tenha conhecimento de situações de abuso.");
-});
-
-document.getElementById('show-como-adotar').addEventListener('click', function() {
-    showInfo("Como Adotar um Pet", "Para adotar um pet, entre em contato com um dos nossos representantes e siga os procedimentos necessários, como a entrevista e a análise do ambiente.");
-});
-
+})
